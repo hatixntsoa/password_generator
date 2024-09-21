9f2ba1bfacd0e2c20dc1429ed3b5ee3fab4bbcfe
Hatix Ntsoa <hatixntsoa@gmail.com>
2024-09-21 15:52:55 +0300

    PyPi Setup

--- setup.py
+++ setup.py
@@ -2,7 +2,19 @@ from setuptools import setup, find_packages
 
 setup(
     name='pypass-tool',
-    version='0.1',
+    version='0.2',
+    description='A CLI tool for password generation',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Hatix Ntsoa',
+    author_email='hatixntsoa@gmail.com',
+    url='https://github.com/h471x/password_generator',
+    license='MIT',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
     packages=find_packages(),
     install_requires=[
         'Flask',
